34718087f7fa5dcf7d51d12d62e24aec3eb66e50
killNit <rileysanti@gmail.com>
2020-08-28 20:42:50 -0600

    Got to level 3 GL

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,36 @@
 class AppointmentsController < ApplicationController
   def index
+    @doctors = @patient.appointment.all 
+    @patient = @doctor.appointment.all
   end
 
   def show
+    @appointment = @patient.appointment.last(params[:id])
   end
 
   def new
+    @appointment = @patient.appointment.new
+  end
+
+  def create 
+    @appointment = @appointment.new(appointment_params)
+    if @appointment.save
+      redirect_to @appointment
+    else 
+      render :new
+    end
   end
 
   def edit
+    @appointment = @patient.appointment.find(params[:id])
   end
+
+  def destroy @appointment = @patient.appointment.find(params[:id])
+   @appointment.destroy
+   redirect_to root
+  end
+
+private
+def appointment_params
+  params.require(:appointment).permit(:doctor.id, :patient.id)
 end
